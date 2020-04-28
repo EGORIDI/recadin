c49872cd54b493c794ad2bc50a848020c3fef08a
EGORIDI <47434302+EGORIDI@users.noreply.github.com>
2020-04-28 19:35:09 -0300

    Build User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Didi", password: "Dissword")
+User.create(username: "Mimi", password: "Missword")
+User.create(username: "Pingo", password: "Pissword")
+User.create(username: "Clori", password: "Clossword")
+User.create(username: "Ana", password: "Anassword")
+User.create(username: "Tata", password: "Tassword")
+User.create(username: "Kiko", password: "Kissword")
+User.create(username: "Ber", password: "Berssword")
+User.create(username: "Nanda", password: "Nassword")
+User.create(username: "Flavio", password: "Flassword")
+User.create(username: "Maria", password: "Massword")
