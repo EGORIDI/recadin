dbd22733d4307cdde527e64ddb69b6ed722ab211
EGORIDI <47434302+EGORIDI@users.noreply.github.com>
2020-05-05 09:20:37 -0300

    Add messages to index and message partial

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,7 @@
 class ChatroomController < ApplicationController
   #
   def index
-    #
+    @messages = Message.all
+    #  update the index action to grab the messages in an instance variable
   end
 end
