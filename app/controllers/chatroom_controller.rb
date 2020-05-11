1327e16573710b40ecd952c1364b822eed8969ce
EGORIDI <47434302+EGORIDI@users.noreply.github.com>
2020-05-11 11:56:10 -0300

    Restrict views at controller layer

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,5 +1,5 @@
 class ChatroomController < ApplicationController
-  #
+  before_action :require_user
   def index
     @messages = Message.all
     #  update the index action to grab the messages in an instance variable
